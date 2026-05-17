ef93596f076aaa0a9f4bb7312eb2db711b00c31b
niru7983 <nir.anjan798304@gmail.com>
2026-05-17 10:10:08 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/hallisanthe/hallisanthe/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.hallisanthe.hallisanthe
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        // Load Home Fragment by default
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_categories -> {
+                    loadFragment(CategoriesFragment())
+                    true
+                }
+                R.id.nav_upload -> {
+                    // Open Upload Activity
+                    startActivity(Intent(this, UploadActivity::class.java))
+                    false // Don't select the item since it's an activity
+                }
+                R.id.nav_wishlist -> {
+                    // Just show home for now or a blank fragment
+                    loadFragment(HomeFragment()) 
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
